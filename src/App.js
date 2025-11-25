e990251894eb1c0670ca43fe7898157eab2510c2
zachf <zach.fraser88@gmail.com>
2025-11-25 07:43:05 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+return (
+<div className="container text-center">
+<h1>Welcome to My Full-Stack App </h1>
+<p>This is a React app styled with Bootstrap 5.</p>
+</div>
+);
+}
+export default App;
